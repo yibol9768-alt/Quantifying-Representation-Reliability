c3d06cb5c68b284e642803f28deb215596e7cb67
yibol9768-alt <yibol9768@Gmail.com>
2026-03-05 17:59:10 +0800

    Remove all files except README.md

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Quantifying Representation Reliability with Heterogeneous Models."""
-
-__version__ = "0.1.0"
--- src/data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Data loading utilities."""
-
-from .datasets import StandardDataset, get_dataloader
-
-__all__ = ["StandardDataset", "get_dataloader"]
